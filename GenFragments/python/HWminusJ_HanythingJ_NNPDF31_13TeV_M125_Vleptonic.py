19618fc3718c98093b795b624a0c7b84eb199965
alintulu <adelina.eleonora.lintuluoto@cern.ch>
2022-01-17 10:20:50 +0100

    add W- and W+ fragments

--- GenFragments/python/HWminusJ_HanythingJ_NNPDF31_13TeV_M125_Vleptonic.py
+++ GenFragments/python/HWminusJ_HanythingJ_NNPDF31_13TeV_M125_Vleptonic.py
@@ -1,11 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
+# link to cards:
+# https://github.com/cms-sw/genproductions/blob/abd5e620174f05eada6dac35f9e366f3019fd6ef/bin/Powheg/production/2017/13TeV/Higgs/WminusHJ_HanythingJ_NNPDF31_13TeV/HWminusJ_HanythingJ_NNPDF31_13TeV_M125_Vleptonic.input
+
 externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
-    args = cms.vstring('root://xrootd-cms.infn.it//store/group/dpg_trigger/comm_trigger/TriggerStudiesGroup/Scouting/Run3/gridpacks/gg_H_quark-mass-effects_slc7_amd64_gcc820_CMSSW_10_6_14_Pyt8240BugFix_my_ggH.tgz'),
+    args = cms.vstring('/cvmfs/cms.cern.ch/phys_generator/gridpacks/UL/13TeV/powheg/V2/HWJ_slc7_amd64_gcc700_CMSSW_10_6_27_WminusH_HToBB_WToLNu_M125_13TeV_powheg/v1/HWJ_slc7_amd64_gcc700_CMSSW_10_6_27_WminusH_HToBB_WToLNu_M125_13TeV_powheg.tgz'),
     nEvents = cms.untracked.uint32(5000),
     numberOfParameters = cms.uint32(1),
     outputFile = cms.string('cmsgrid_final.lhe'),
-    scriptName = cms.FileInPath('SampleGeneration/GenFragments/data/run_generic_tarball_xrootd.sh')
+    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh')
 )
 
 import FWCore.ParameterSet.Config as cms
@@ -19,14 +22,14 @@ generator = cms.EDFilter("Pythia8HadronizerFilter",
                          pythiaPylistVerbosity = cms.untracked.int32(1),
                          filterEfficiency = cms.untracked.double(1.0),
                          pythiaHepMCVerbosity = cms.untracked.bool(False),
-                         comEnergy = cms.double(14000.),
+                         comEnergy = cms.double(13000.),
                          PythiaParameters = cms.PSet(
         pythia8CommonSettingsBlock,
         pythia8CP5SettingsBlock,
         pythia8PSweightsSettingsBlock,
         pythia8PowhegEmissionVetoSettingsBlock,
         processParameters = cms.vstring(
-            'POWHEG:nFinal = 1',   ## Number of final state particles
+            'POWHEG:nFinal = 3',   ## Number of final state particles
                                    ## (BEFORE THE DECAYS) in the LHE
                                    ## other than emitted extra parton
             '25:m0 = 125.0',
@@ -43,5 +46,3 @@ generator = cms.EDFilter("Pythia8HadronizerFilter",
                          )
 
 ProductionFilterSequence = cms.Sequence(generator)
-
-
