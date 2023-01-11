de7c992b42e85c8b17c114eaef86466c9cdd14e7
alintulu <adelina.eleonora.lintuluoto@cern.ch>
2023-01-11 16:44:28 +0100

    add UL comparisons for neutrino final states processes

---
+++ GenFragments/run/WminusH_HToBB_WToLNu_M-125_TuneCP5_13TeV-powheg-pythia8_UL18.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cmsDriver.py \
+    SampleGeneration/GenFragments/python/WminusH_HToBB_WToLNu_M-125_TuneCP5_13TeV-powheg-pythia8_UL18.py \
+    --python_filename SampleGeneration/Analysis/config/WminusH_HToBB_WToLNu_M-125_TuneCP5_13TeV-powheg-pythia8_UL18.py \
+    --eventcontent NANOAODGEN \
+    --datatier NANOAOD \
+    --fileout file:WminusH_HToBB_WToLNu_M-125_TuneCP5_13TeV-powheg-pythia8_UL18.root \
+    --conditions auto:phase1_2022_realistic \
+    --step LHE,GEN,NANOGEN \
+    --geometry DB:Extended \
+    --era Run3 \
+    --no_exec \
+    --mc \
+    -n 1
---
+++ GenFragments/run/WplusH_HToBB_WToLNu_M-125_TuneCP5_13TeV-powheg-pythia8_UL18.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cmsDriver.py \
+    SampleGeneration/GenFragments/python/WplusH_HToBB_WToLNu_M-125_TuneCP5_13TeV-powheg-pythia8_UL18.py \
+    --python_filename SampleGeneration/Analysis/config/WplusH_HToBB_WToLNu_M-125_TuneCP5_13TeV-powheg-pythia8_UL18.py \
+    --eventcontent NANOAODGEN \
+    --datatier NANOAOD \
+    --fileout file:WplusH_HToBB_WToLNu_M-125_TuneCP5_13TeV-powheg-pythia8_UL18.root \
+    --conditions auto:phase1_2022_realistic \
+    --step LHE,GEN,NANOGEN \
+    --geometry DB:Extended \
+    --era Run3 \
+    --no_exec \
+    --mc \
+    -n 1
